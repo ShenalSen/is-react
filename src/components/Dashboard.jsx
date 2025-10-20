04388214fc3445b187db354784c0627f45b9879e
ShenalSen <shenal.unc555@gmail.com>
2025-10-20 10:31:33 +0530

    feat: add Dashboard component with protected access message

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>This is a protected dashboard page. You can only see this if you are authenticated.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
