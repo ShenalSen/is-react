ab184c4550e968cbe2445ddb3c83c61ce70ee463
ShenalSen <shenal.unc555@gmail.com>
2025-10-20 10:31:40 +0530

    feat: add Profile component with protected access for user account details

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>This is a protected profile page where you can view and edit your account details.</p>
+    </div>
+  );
+};
+
+export default Profile;
